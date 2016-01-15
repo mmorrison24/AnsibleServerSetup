ba792b394ab6c79a564902aa1d0ae919b8b5abcb
Administrator <admin@example.com>
2016-01-15 13:35:27 -0500

    Adding Files

---
+++ Vagrant/v_managebox/setup_ansible.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## Enable PPA and install Ansible
+
+# from http://www.ansibleworks.com/docs/intro_installation.html#latest-releases-via-apt-ubuntu
+
+apt-get update --yes
+apt-get install --yes python-software-properties
+#add-apt-repository --yes ppa:rquillo/ansible
+apt-add-repository ppa:ansible/ansible
+apt-get update --yes
+apt-get install --yes ansible
